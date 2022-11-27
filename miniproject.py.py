72f67af3c0d2c3e1e244a3f91c0a8fa41c98f895
shashikant Borude <119213408+shashikant62@users.noreply.github.com>
2022-11-27 21:51:06 +0530

    Add files via upload

---
+++ miniproject.py.py
@@ -0,0 +1,18 @@
+#calci
+a=int(input('enter a='))
+b=int(input('enter b='))
+print('add=1')
+print('subtract=2')
+print('multy=3')
+print('div=4')
+c=int(input())
+if(c==1):
+    print(a+b)
+elif(c==2):
+    print(a-b)
+elif(c==3):
+    print(a*b)
+elif(c==4):
+    print(a/b)
+else:
+    print('SOMTHING WENT WRONG')
